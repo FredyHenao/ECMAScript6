bdb2a2a7eb9d2021dbed01d40c0bcb82fefb1357
FredyHenao <jhongallego45y@hotmail.com>
2017-10-24 22:04:47 -0500

    Video 5
    Declaración de constantes

--- variables.js
+++ variables.js
@@ -9,13 +9,36 @@
  *
  * en ecmascript6 no hay re-declaracion de variables
 **/
-let mensaje = "1234";
+/*let mensaje = "1234";
 
 if(1===1){
     let mensaje = "Hola Mundo";
     console.log(mensaje);
 }
 
-console.log(mensaje);
+console.log(mensaje);*/
+
+/* Declaración de variables constantes
+ *  es buena practica que las constantes se escriban en mayuscula
+ *  tienen que inicializarce en el momento que se declara
+ * no se le pueden reasignar valores
+ *  se pueden declarar objetos
+ *  no se puede volver a declarar
+ */
+
+const IMPUESTO_SV = 15.5;
+
+const PERSONA = {
+    nombre: "Fredy",
+    apellido: "Henao"
+}
+PERSONA.nombre = "Jhon";
+
+if( true ){
+    const IMPUESTO_SV = 19.5;
+    console.log(IMPUESTO_SV);
+}
+
+console.log(IMPUESTO_SV);
 
 
