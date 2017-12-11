68692ec0ea20050d65dac726801c472e90ea828a
FredyHenao <jhongallego45y@hotmail.com>
2017-12-11 18:26:57 -0500

    Video 8
    segmento de caracteres

---
+++ segCaracteres.js
@@ -0,0 +1,7 @@
+var saludo = "Hola Mundo!";
+
+console.log( saludo.startsWith("H"));//evaluar la primera palabra o letra
+console.log( saludo.endsWith("Hola"));//evaluar la última palabra o letra
+console.log( saludo.includes("x"));//evaluar si la letra se encuentra en el texto
+
+console.log( saludo.startsWith("Mu",5));//evaluar desde una posicion en adelante 
