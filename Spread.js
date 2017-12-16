82c89c3f6501e7987b3b8ac77c1bc4a489433017
FredyHenao <jhongallego45y@hotmail.com>
2017-12-16 00:11:09 -0500

    Video 18
    Diferencia entre SPREAD y REST

--- Spread.js
+++ Spread.js
@@ -8,3 +8,28 @@ let numeros = [1,2,3,4,19,349,234];
 let max = Math.max(...numeros);
 
 console.log(max);
+
+/* Diferencia entre Spread y Rest
+* REST: Junta los elementos en un arreglo
+* SPREAD: Esparce los elementos como si fueran enviados de forma separada*/
+
+function saludarRest(saludo, ...nombres) {
+    for(i in nombres ){
+        console.log(`${saludo} ${nombres}`);
+    }
+}
+
+function saludarSpread(saludo, ...nombres) {
+    console.log(`${saludo} ${nombres}.`);
+}
+
+saludarRest("Hola","Fernando","Maria");
+
+let personas = ["Fernando","Maria"];
+saludarSpread("Que tal",personas);
+
+
+let partes = ["brazos","piernas"];
+let cuerpo = ["cabeza",...partes,"pies"];
+
+console.log(cuerpo);
