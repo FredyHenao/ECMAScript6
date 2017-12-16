26b55a48f1f56a98fd787f20a5cb39663b30619d
FredyHenao <jhongallego45y@hotmail.com>
2017-12-15 22:38:55 -0500

    Video 12
    Valores raw con template literal

--- LTTag.js
+++ LTTag.js
@@ -16,3 +16,13 @@ let unidades = 5,
 let mensaje = etiqueta`${unidades} lapices cuestan ${unidades * costo_unitario} pesos.`;
 
 console.log(mensaje);
+
+// usar valores "raw"(crudos) en template literal
+let mensaje1 = `Hola \nMundo\\`,
+    mensaje2 = String.raw`Hola \nMundo\\`;
+
+//var mensaje3 = String.raw'Hola \nMundo\\';
+
+console.log(mensaje1);
+console.log(mensaje2);
+//console.log(mensaje3);
