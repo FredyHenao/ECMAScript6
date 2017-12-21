dfd38d76bc80d222d862f3be72690f2ed642e7ac
FredyHenao <jhongallego45y@hotmail.com>
2017-12-20 23:52:00 -0500

    Video 30
    Nuevo metodo Object.assign

--- newObject.js
+++ newObject.js
@@ -1,4 +1,4 @@
-
+/*Nuevo metodo Object.is*/
 console.log( +0 == -0 );
 console.log( +0 === -0 );
 console.log( Object.is(+0, -0) );
@@ -21,3 +21,28 @@ console.log( "=========" );
 
 console.log( Object.is(5 ,5));
 console.log( Object.is(5 ,"5") );
+
+/*Nuevo metodo Object.assign*/
+//pasar propiedades de una objeto a otro sin utilizar herencia
+
+//ES5
+function mezclar( objReceptor, objDonador) {
+    Object.keys( objDonador ).forEach( function (key) {
+        objReceptor[key] = objDonador[key];
+    } );
+
+    return objReceptor;
+}
+
+var objReceptor = {};
+var objDonador = {
+  get nombre(){
+      return "mi-archivo.js"
+  }
+};
+
+console.log( mezclar( objReceptor, objDonador ) );
+
+//ES6
+console.log("=======ES6")
+console.log( Object.assign( objReceptor, objDonador ) );
