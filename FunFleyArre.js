64c6e9abac68ff5887b15e5c7058d8766d0dba3c
FredyHenao <jhongallego45y@hotmail.com>
2017-12-20 21:36:18 -0500

    Video 25
    Identificando funciones de flecha y otros ejemplos

--- FunFleyArre.js
+++ FunFleyArre.js
@@ -8,3 +8,20 @@ var ordenadoES5 = arreglo.sort(function (a,b) {
 let ordenadoES6 = arreglo.sort((a,b)=>a-b);
 
 console.log( ordenadoES6 );
+
+/*Identificando funciones de flecha y otros ejemplos*/
+
+var restar = (a,b) => a-b;
+
+console.log( typeof restar );
+console.log( restar instanceof Function );
+
+// var restar2 = new restar(1,2);
+
+function ejemplo(x,y) {
+    ((a,b) => {
+        console.log( arguments );
+    })();
+}
+
+ejemplo(10.20);
