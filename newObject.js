fe63b5927d2345f323eda1a2042b99dc37188023
FredyHenao <jhongallego45y@hotmail.com>
2017-12-21 00:04:36 -0500

    Video 31
    Orden de la enumeración de las propiedades de los objetos

--- newObject.js
+++ newObject.js
@@ -46,3 +46,32 @@ console.log( mezclar( objReceptor, objDonador ) );
 //ES6
 console.log("=======ES6")
 console.log( Object.assign( objReceptor, objDonador ) );
+
+/*Orden de la enumeración de las propiedades de los objetos
+* todas las llaves van en orden ascendente
+* todas las llaves tipo string, van ordenadas en la manera que fueron agregadas al objeto
+* todos los símbolos en el orden que fueron agregados al objeto*/
+
+var objeto = {
+    c: 1,
+    0: 1,
+    x: 1,
+    15: 1,
+    r: 1,
+    3: 1,
+    b: 1
+};
+
+objeto.d = 1;
+objeto["2"] = 1;
+objeto["a"] = 1;
+
+console.log( Object.getOwnPropertyNames( objeto ).join(",") );
+
+console.log( Object.keys( objeto ) );
+
+console.log( JSON.stringify( objeto ) );
+
+for( i in Object.keys( objeto )){
+    console.log( Object.keys( objeto )[i] );
+}
