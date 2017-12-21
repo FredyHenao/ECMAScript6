cd6ee94ecf359b0042c92cdfe047b4f293907915
FredyHenao <jhongallego45y@hotmail.com>
2017-12-20 22:42:07 -0500

    Video 26
    Extensión de objetos literales

---
+++ ObjectLite.js
@@ -0,0 +1,21 @@
+/*Objetos literales en ES6
+* Este tipo de estructura es el patrón más utilizado en JavaScript. (JSON se construyó de esta sintaxis).
+* Dichos objetos se encuentran en casi cualquier script en el internet, casi el 99% de los programadores los utilizan en algún punto.*/
+
+function crearPersona( nombre, apellido, edad ) {
+    return{
+        /*ES5
+        nombre:nombre,
+        apellido:apellido,
+        edad:edad
+        */
+        //ES6
+        nombre,
+        apellido,
+        edad
+    }
+}
+
+var fredy = crearPersona("Fredy","Henao",30);
+
+console.log( fredy );
