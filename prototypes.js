7d31331482b2df2e6faebd6e72a4ea34d2a13233
FredyHenao <jhongallego45y@hotmail.com>
2017-12-22 18:41:00 -0500

    Video 38
    destructuración de parámetros por defecto

--- prototypes.js
+++ prototypes.js
@@ -135,3 +135,23 @@ let [ colorPrincipal, ...demasColores ] = colores2;
 
 console.log( colorPrincipal );
 console.log( demasColores );
+
+/*destructuración de parámetros por defecto*/
+
+function crearJugador( nickname,
+                       {hp, sp, clase} = {hp:100, sp:50, clase:"Mago"}
+) {
+
+    //opciones = opciones || {};
+    //let hp = opciones.hp,
+        //sp = opciones.sp,
+        //clase = opciones.clase;
+
+    console.log( nickname, hp, sp, clase );
+}
+
+crearJugador( "Strider",{
+   hp:500,
+   sp: 100,
+   clase: "Warrior"
+});
