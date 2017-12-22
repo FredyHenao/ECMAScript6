297258e6e7419e4c4690851dd7cb90894ce35aab
FredyHenao <jhongallego45y@hotmail.com>
2017-12-22 18:07:54 -0500

    Video 35
    Destructuración de objetos anidados

--- prototypes.js
+++ prototypes.js
@@ -65,3 +65,40 @@ let ajustes = {
 let { nombre, email, facebook = "jajaaj", twitter:cuentaTwi = "fre45"} = ajustes;
 
 console.log( cuentaTwi, facebook );
+
+/*Destructuración de objetos anidados*/
+
+let autoGuardado = {
+    archivo: "app.js",
+    cursor: {
+        linea: 7,
+        columna: 16
+    },
+    ultimoArchivo:{
+        archivo: "index.html",
+        cursor: {
+            linea: 8,
+            columna: 20
+        }
+    },
+    otroNodo:{
+        subNodo:{
+            cursor: {
+                linea: 11,
+                columna: 11
+            }
+        }
+    }
+}
+
+let { cursor:cursorActivo } = autoGuardado;
+console.log( cursorActivo );
+
+let { ultimoArchivo:{ cursor:ultimoArchivo } } = autoGuardado;
+console.log( ultimoArchivo );
+
+let { otroNodo:{ subNodo:{ cursor:otroSuperNodo } } } = autoGuardado;
+console.log( otroSuperNodo );
+
+let otroSuperNodo2 = autoGuardado.otroNodo.subNodo.cursor;
+console.log( otroSuperNodo2 );
