b2e9f8bff2d824381d4f2b2652a6444a4a55b3de
FredyHenao <jhongallego45y@hotmail.com>
2017-12-28 15:05:19 -0500

    Video 50
    WeekSets

--- Set.js
+++ Set.js
@@ -61,3 +61,14 @@ function  eliminaDuplicados( item ) {
     return [... new Set(item)];
 
 }
+
+/*WeekSets
+* Trabajan de una manera similar a los Sets
+* Características
+    1. En un weekset, ADD(), HAS(), REMOVE(), dan un error si enviamos como parámetro algo que no sea un objeto.
+    2. No tiene manera de hacer repeticiones o ciclos for in.
+    3. Los weeksets no tienen keys(), values(), por lo que no hay manera vía programación de saber cuantos elementos hay dentro
+    4. No tienen un for-each tampoco
+    5. No tienen propiedad size
+    */
+
