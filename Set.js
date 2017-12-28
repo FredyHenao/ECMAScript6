2e603e489f3518606b9261c6d18d9e60610b7099
FredyHenao <jhongallego45y@hotmail.com>
2017-12-28 14:10:29 -0500

    Video 46
    Creando Sets

---
+++ Set.js
@@ -0,0 +1,21 @@
+/*Los Set
+los sets son una lista ordenada de valores sin duplicados.
+Permiten un rápido acceso a la data que contienen
+* Hostoricamente, JavaScript sólo ha tenido un tipo de colección de datos los array
+* Y aunque muchos digan que los objetos son una colección de pares de valores, no son realmente una colección de datos*/
+
+let items = new Set( [1,2,3,4,5,6,7,7,7,7] );
+/*add->agregar un elemento. has->saber si existe el elemento*/
+console.log( items.has(7) );
+
+/*items.add(10);
+items.add(11);
+items.add(8);
+items.add(7);
+items.add(7);
+items.add(7);
+items.add("7");
+
+console.log( items.size );*/
+
+console.log( items );
