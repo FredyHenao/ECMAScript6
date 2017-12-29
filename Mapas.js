87a016a7c505f71666cef74610d496f7e9614a41
FredyHenao <jhongallego45y@hotmail.com>
2017-12-29 13:40:47 -0500

    Video 55
    Nuevo ciclo FOR-OF

--- Mapas.js
+++ Mapas.js
@@ -43,3 +43,47 @@ mapa2.forEach( (valor, llave, mapaOrigen) => {
     console.log( ` Llave: ${llave}, valor: ${valor} ` );
     console.log( mapaOrigen );
 } );
+
+/*Nuevo ciclo FOR-OF-> ciclo especializado para las colecciones*/
+
+/*let numeros = [100,20,30,50,200];
+
+for(let i = 0; i < numeros.length; i++){
+    console.log( numeros[i] );
+}
+
+for(let i in numeros){
+    console.log( numeros[i] );
+}
+
+for( let numero of numeros ){
+    console.log( numero );
+}*/
+
+let personas = [
+    { nombre: "Fredy",edad:21 },
+    { nombre: "Edilma",edad:54 },
+    { nombre: "Mario",edad:34 },
+    { nombre: "Victor",edad:34 },
+    { nombre: "Nestor",edad:45 }
+];
+
+for( let per of personas){
+    console.log( per.nombre, per.edad );
+}
+
+let personas1 = new Set();
+
+personas1.add( { nombre:"Fredy", edad:21 } );
+personas1.add( { nombre:"Jhon", edad:21 } );
+personas1.add( { nombre:"Gerardo", edad:31 } );
+
+for( let per of personas1){
+    console.log( per.nombre, per.edad );
+}
+
+let mapa3 = new Map( [ ["nombre","Jhon"], ["apellido","Henao"] ] );
+
+for( let detalle of mapa3){
+    console.log( detalle );
+}
